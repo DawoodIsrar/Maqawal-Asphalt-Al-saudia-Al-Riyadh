4eaca59605b0335751ca3bde378479e7cd3af046
DawoodIsrar <engrdawoodisrar@gmail.com>
2025-08-02 22:04:24 +0500

    feat: resolve issue

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,13 @@ import About from '../components/About'
 import Benefits from '../components/Benefits'
 import Choose from '../components/Choose'
 import Contact from '../components/Contact'
-import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 import Latest from '../components/Latest'
-import News from '../components/News'
 import Numbers from '../components/Numbers'
 import Products from '../components/Projects'
 import Reviews from '../components/Reviews'
 import ContactButtons from '../components/Contactbuttons'
+
 
 export default function Home() {
   return (
